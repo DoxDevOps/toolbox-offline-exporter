95460bbde76169ed8d487d8acdc92b96f5561d82
YOUR NAME <YOUR@EMAIL.com>
2022-04-25 12:10:06 +0200

    added config file validation

--- utils/setup_toolbox.py
+++ utils/setup_toolbox.py
@@ -1,1 +1,14 @@
 # coding=utf-8
+import json
+
+
+def validate_config_file():
+    """
+    validates if data is in the correct format
+    :return: Return False when the file is not correct
+    """
+    try:
+        with open('config/config.json') as f:
+            return json.load(f)
+    except ValueError as e:
+        return False
