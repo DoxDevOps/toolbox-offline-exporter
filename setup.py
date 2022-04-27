0eaabfd313ee535612a37ffb04daef88171cecad
YOUR NAME <YOUR@EMAIL.com>
2022-04-27 16:13:36 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #! /usr/bin/python
 import os
-from utils.setup_toolbox import get_facility_name, mac_address
+from utils.setup_toolbox import mac_address
 
 
 def configure_site():
     """
-    sets up toolbox
-    :return: true
+    configures toolbox
+    :return:
     """
     # Configures the site
     # install pip
-    print(" Step 1 : Update laptop")
+    print("Step 1 : Update laptop")
     answer = os.system("sudo apt-get update")
     print("Step 2: Install Pip.")
     answer1 = os.system("sudo apt install python-pip")
@@ -18,25 +18,25 @@ def configure_site():
     print("Step3 : install python environment")
     os.system("sudo apt install virtualenv")
     os.system("virtualenv flask")
-    print("creating Toolbox Service")
-    # here is the code for creating the site.
-    print("********************")
-    print("SET UP FACILITY DETAILS")
-    get_facility_name()
-    print("SITE IS NOW CONFIGURED !!!")
-    print("********************")
-    os.system("sudo cp toolbox.service /etc/systemd/system/")
-    os.system("sudo systemctl daemon-reload && sudo systemctl start toolbox && sudo systemctl enable toolbox")
     print("*********** SETTING FACILITY DETAILS *****************")
     os.system(". flask/bin/activate && python -c 'import setup_facility_details;  "
               "setup_facility_details.get_facility_name()'")
     mac = mac_address()
     print(mac)
     print("*********** END - Facility Configured Successfully *****************")
+    print("creating Toolbox Service")
+    # here is the code for creating the site.
+    os.system("sudo cp toolbox.service /etc/systemd/system/")
+    os.system("sudo systemctl daemon-reload && sudo systemctl start toolbox && sudo systemctl enable toolbox")
+    print("FINISHED :creating Toolbox Service")
     return True
 
 
 def main():
+    """
+    startup function
+    :return: boolean
+    """
     configure_site()
     return True
 
