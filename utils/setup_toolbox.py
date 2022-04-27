a02693369d7a54cacc18d093c05fa144e73e075b
YOUR NAME <YOUR@EMAIL.com>
2022-04-27 15:27:13 +0200

    configured config file

--- utils/setup_toolbox.py
+++ utils/setup_toolbox.py
@@ -1,2 +1,131 @@
 # coding=utf-8
+#!/bin/bash
+import json
+import urllib2
+import re
+import uuid
 
+
+
+def get_facility_name():
+    """
+    starting point. prompts user to enter a suggested facility name
+    :return: Boolean (just a checker )
+    """
+    facility_name = raw_input("Enter Facility Name Please : ")
+    search_facilities(facility_name)
+    return True
+
+
+def search_facilities(facility_name):
+    """
+    get suggested name and search in a remote server
+    :param facility_name: string
+    :return: boolean (just a checker)
+    """
+    settings = load_settings("config/.json")
+    url = settings["endpoint"]
+    token = settings["token"]
+    json_dict = {'site_name': facility_name}
+
+    # convert json_dict to JSON
+    json_data = json.dumps(json_dict)
+
+    # Creating a Post request
+    req = urllib2.Request(url, json_data)
+    req.get_method = lambda: 'GET'
+    req.add_header('Content-type', 'application/json')
+    req.add_header('Accept', 'text/plain')
+    req.add_header('Authorization', token)
+    r = urllib2.urlopen(req)
+    results = r.read()
+    if len(results) != 2:
+        display_facilities(results)
+    else:
+        print("\n No match Found, Please try again :")
+        get_facility_name() # start all over
+    return True
+
+
+def display_facilities(facilities):
+    """
+    displays all possible sites
+    :param facilities: json object
+    :return:
+    """
+    facilities = json.loads(facilities)
+    counter = 0
+    for facility in facilities:
+        counter += 1
+        print(str(counter) + " " + facility['fields']['name'])
+    select_facility(facilities, counter)
+    return True
+
+
+def select_facility(facilities, counter):
+    """
+    gives a user an option to select sites from a given list
+    :type counter: int
+    :type facilities: json object
+    :param facilities: json object
+    :param counter: a counter on the number of sites retrieved
+    """
+    while True:
+        try:
+            facility_number = int(raw_input("\nConfirm Facility Name by Entering a Number : ")) - 1
+
+            if facility_number + 1 > counter:
+                print("The number selected is not on the list, Please try again.")
+                continue
+            if facility_number < 0:
+                print("Please Select a POSITIVE number")
+                continue
+            save_facility(facilities, facility_number)
+        except ValueError:
+            print("\nPlease enter a valid number")
+            continue
+        else:
+            break
+
+
+def save_facility(facilities, facility_number):
+    """
+    saves facility details after configuration
+    :type facilities: json object
+    :type facility_number: int
+    :param facilities:
+    :param facility_number: number se
+    :return: Boolean
+    """
+    selected_facility = facilities[facility_number]['fields']['name']
+    site_data = {"apps": facilities[facility_number]['fields']['apps'], "name": selected_facility,
+                 "uuid": facilities[facility_number]['fields']['uuid']}
+    print("Selected District : " + selected_facility)
+    facility_details().save_facility_details(site_data)  # then save the details in config file
+    return True
+
+
+def mac_address():
+    """
+    gets machine MAC address
+    :return:
+        mac_address : mac address of a machine
+    """
+    # convert to hex and separate two figures with :
+    mac_address = ':'.join(re.findall('..', '%012x' % uuid.getnode()))  # type: Union[Union[str, unicode], Any]
+    return mac_address
+
+
+def send_mac_address():
+    return True
+
+
+def load_settings(location):
+    """
+    a function that loads settings (token and endpoint)
+    :type location: string
+    :return: json object
+    """
+    with open(location) as f:
+        settings = json.load(f)
+    return settings
--- utils/validate_emr_data.py
+++ utils/validate_emr_data.py
@@ -12,3 +12,24 @@ def validate_config_file():
             return json.load(f)
     except ValueError as e:
         return False
+
+
+def save_facility_details(site_data):  # this function will be called whe we implement a POP UP MENU for Site
+    """
+    gets facility details as dictionary and saves the file as a json file
+    :param site_data:
+    :return:
+    """
+    # details to be entered on the web browser
+    if site_data["apps"][0] == "Point of Care":
+        app_id = 1
+    else:
+        app_id = 2
+
+    site_name = site_data["name"]
+    uuid = site_data["uuid"]
+    with open("config/config.json", "w") as data:
+        information = {"uuid": uuid, "app_id": app_id, "site_name": site_name}
+        data.write(json.dumps(information))
+        data.close()
+    return True
