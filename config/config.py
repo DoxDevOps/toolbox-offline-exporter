bf158ac207b9c60fa756fd88babdaa9cf4b579f2
YOUR NAME <YOUR@EMAIL.com>
2022-06-16 16:33:31 +0200

    added added module config

--- config/config.py
+++ config/config.py
@@ -4,3 +4,13 @@ data = \
         "config": "config/config.json",
         "apps_loc": "config/apps.json"
     }
+modules = \
+    {
+        "ANC": "/var/www/BHT-Core/apps/HTS",
+        "OPD": "/var/www/BHT-Core/apps/HTS",
+        "MATERNITY": "/var/www/BHT-Core/apps/HTS",
+        "HTS": "/var/www/BHT-Core/apps/HTS",
+        "1": "/var/www/BHT-Core/apps/HTS",
+        "2": "/var/www/BHT-Core/apps/HTS"
+
+    }
