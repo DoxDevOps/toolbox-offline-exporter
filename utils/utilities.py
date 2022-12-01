aa6a78f5a8ad8a51b54d514309f57222f6a83f5a
dominickasanga <dominickasanga@gmail.com>
2022-12-01 16:51:33 +0200

    modified:   .gitignore modified:   app.py deleted:    config/apps.json deleted:    config/config.json modified:   reconfig.py modified:   setup.py modified:   templates/static/images/toolbox.png modified:   utils/setup_toolbox.py modified:   utils/utilities.py

--- utils/utilities.py
+++ utils/utilities.py
@@ -50,3 +50,24 @@ def get_request(url, token, body):
     results = json.dumps(results.json())
 
     return results
+
+def get_host_serial():
+    """
+    a function that read serial stored in a file
+    :return: serial as str
+    """
+    file = open("config/___rapt___.txt","r")
+    serial = file.read()
+    file.close()
+    return serial.strip()
+
+def save_host_serial(serial):
+    """
+    a function that stores serial in a file
+    :param serial: serial to be stored
+    :return: serial as str
+    """
+    file = open("config/___rapt___.txt","w")
+    file.write(serial.strip())
+    file.close()
+    return serial.strip()
