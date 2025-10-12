5ea0b2656d7a5b577ad3dfa7b969c98ee2cdc854
lwara <lwaratiwo94@gmail.com>
2025-10-12 14:06:27 +0200

    edited the startup service

--- startup_exporter.py
+++ startup_exporter.py
@@ -1,30 +1,36 @@
 import json
 import requests
-from config.config import data
-from utils.export_emr_data import check_installation_folders, get_emr_versions 
+from config.config import data, setup_emr, bridge_endpoint
+from utils.export_emr_data import  get_emr_version 
 from utils.validate_emr_data import validate_config_file
 
 def collect_and_send():
     if not validate_config_file(data["config"]) or not validate_config_file(data["apps_loc"]):
         return
 
-    emr_data = check_installation_folders(data["apps_loc"])
-    emc_data = get_emr_versions(data["emc_loc"])    
-    uuid = data["uuid"]
-
+    
+    emc_data = setup_emr["emc"]
+    emr_data = get_emr_version(emc_data)
+    config_file_data = validate_config_file(data["config"])
+    uuid = config_file_data["uuid"]
+    
     final_emr_data = {
         "uuid": uuid,
-        "app_dir": emr_data,
-        "version": emc_data
+        "app_dir": emc_data,
+        "version": emr_data
     }
 
+    print(final_emr_data)
     # Send to endpoint
-    endpoint = data["bridge_endpoint"]  # <-- change this to your endpoint
+    
     try:
-        response = requests.post(endpoint, json=final_emr_data)
+        response = requests.post(bridge_endpoint, json=final_emr_data)
         print("Status:", response.status_code)
     except Exception as e:
         print("Failed to send data:", e)
 
 if __name__ == "__main__":
     collect_and_send()
+
+
+
